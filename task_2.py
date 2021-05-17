07bfc40c9211b1915e830266286f4e3cf87c99f3
romanpovzyk <roman.povzyk@gmail.com>
2021-05-17 20:55:02 +0300

    First commit

---
+++ task_2.py
@@ -0,0 +1,12 @@
+# using sep
+print(0, 95, 129, 12, 64, sep="-")
+print(1, 2, 3, 4, 5, sep="*")
+
+# using \n
+print('Roman', end=" ")
+print('Povzyk')
+
+# using more \n
+print(end="\n")
+print('Roman', end="\n\n\n")
+print('Povzyk')
---
+++ task_3.py
@@ -0,0 +1,12 @@
+# make O
+print('#' * 9, end='\n')
+for i in range(3):
+    print('#', '#', sep="\t\t")
+print('#' * 9, end='\n\n')
+
+# make H
+for i in range(2):
+    print('#', '#', sep="\t\t")
+print('#' * 9, end='\n')
+for i in range(2):
+    print('#', '#', sep="\t\t")
